8a8b3dc3abf8e80816c9c1293bc2d9a169740ae4
OOLebedenko <oolebedenko@gmail.com>
2023-09-04 14:17:38 +0300

    fixed extraction last frame in build_prod.sh: build_box_prod.sh

--- md_setup/md_protocol/build.sh
+++ md_setup/md_protocol/build.sh
@@ -1,24 +1,30 @@
 #! /bin/bash -f
 
 ######################################################################################################################################################################################
-#                                                   1. solvate system
+#                                                   1. extract last frame from trajectory of tails equilibration
+######################################################################################################################################################################################
+
+$AMBERHOME/bin/cpptraj -i 0_prepare/cpptraj_extract_last_frame.in
+
+######################################################################################################################################################################################
+#                                                   2. solvate system
 ######################################################################################################################################################################################
 
 $AMBERHOME/bin/tleap -s -f 1_build/tleap_draft_box.rc
 
 
 ######################################################################################################################################################################################
-#                                                   2. calculate number of K+ and Cl- ions needed to neutralize system and to match the target salt concentration
+#                                                   3. calculate number of K+ and Cl- ions needed to neutralize system and to match the target salt concentration
 ######################################################################################################################################################################################
 
 # target KCl concentration
-target_kcl_concentration=0.15  # mol/l
+target_Kcl_concentration=0.15  # mol/l
 
 # avogadro_constant
 avogadro_constant=6.022140857e+23 # mol-1
 
 # number of KCl molecules in the 1 L KCl solution
-n_kcl_in_one_liter=$(awk "BEGIN {print $target_kcl_concentration * $avogadro_constant; exit}")
+n_kcl_in_one_liter=$(awk "BEGIN {print $target_Kcl_concentration * $avogadro_constant; exit}")
 
 # The density of water is ~0.997 g/mL at the room temperature, and thus, the weight of 1 L water is ~997 g.
 # Since the weight of 1 mol H2O is ~18.02 g
@@ -43,10 +49,10 @@ n_k=$(printf %1.0f $n_k)
 n_cl=$(printf %1.0f $n_cl)
 
 ######################################################################################################################################################################################
-#                                                       3. resolvate system to match experimental KCl concentration
+#                                                       4. resolvate system to match experimental KCl concentration
 ######################################################################################################################################################################################
 
-# # save input file for tleap
+# save input file for tleap
 cp 1_build/tleap_draft_box.rc 1_build/tleap_box.rc
 sed -i s/draft_//g 1_build/tleap_box.rc
 sed -i "s/addions mol K+ 0/addions mol K+ ${n_k}/g" 1_build/tleap_box.rc
@@ -55,7 +61,7 @@ sed -i "s/addions mol Cl- 0/addions mol Cl- ${n_cl}/g" 1_build/tleap_box.rc
 $AMBERHOME/bin/tleap -s -f 1_build/tleap_box.rc
 
 #####################################################################################################################################################################################
-#                                                      4. strip solvent from box.prmtop --> nucleosome.prmtop
+#                                                      5. strip solvent from box.prmtop --> nucleosome.prmtop
 #####################################################################################################################################################################################
 
 $AMBERHOME/bin/cpptraj -i 1_build/cpptraj_strip_solvent.in
